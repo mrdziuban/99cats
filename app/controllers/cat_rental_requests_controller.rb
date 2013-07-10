451c922a5fc925184eacf0fba95bf9d793060cb3
Samir and Matt <samir+matt@appacademy.io>
2013-07-10 11:44:36 -0400

    Add #show method to CatRentalRequestsController and corresponding view

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -2,4 +2,9 @@ class CatRentalRequestsController < ApplicationController
   def index
     @crrs = CatRentalRequest.find_all_by_cat_id(params[:cat_id])
   end
+
+  def show
+    @cat = Cat.find_by_id(params[:cat_id])
+    @crr = CatRentalRequest.find(params[:id])
+  end
 end
