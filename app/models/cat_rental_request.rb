f69c1dec0f78e598fa1e317b67db0edf760ab639
Samir and Matt <samir+matt@appacademy.io>
2013-07-10 15:58:24 -0400

    Add CatRentalRequest#approve

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -2,7 +2,8 @@ class DateValidator < ActiveModel::Validator
   def validate(record)
     crrs = CatRentalRequest.find_all_by_cat_id(record.cat_id)
     crrs.each do |crr|
-      if record.begin_date < crr.end_date && record.end_date > crr.begin_date
+      if (record.begin_date < crr.end_date && record.end_date > crr.begin_date) &&
+        crr.status == "approved"
         record.errors[:begin_date] << "Cannot have overlapping dates"
       end
     end
@@ -14,4 +15,16 @@ class CatRentalRequest < ActiveRecord::Base
   validates_with DateValidator
 
   belongs_to :cat
+
+  def approve
+    status = "approved"
+
+    crrs = CatRentalRequest.find_all_by_cat_id(cat_id)
+    crrs.each do |crr|
+      if begin_date < crr.end_date && end_date > crr.begin_date
+        crr.status = "denied"
+        crr.save
+      end
+    end
+  end
 end
