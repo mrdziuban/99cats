7d3116ed21fbab4ed3d9af50ae3ebd785dcd79fc
Samir and Matt <samir+matt@appacademy.io>
2013-07-10 17:06:04 -0400

    Remove unnecessary #new method from UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-  def new
-  end
-
   def create
     user = User.create(params[:user], session_token: SecureRandom.urlsafe_base64(8))
     redirect_to cats_url
