044d0fde7cdafc8569d55345164bfb3b5f6332bf
Samir and Matt <samir+matt@appacademy.io>
2013-07-10 10:36:16 -0400

    Add #update method for Cat model

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -19,4 +19,13 @@ class CatsController < ApplicationController
     @colors = ColorValidator::COLORS
     @cat = Cat.find(params[:id])
   end
+
+  def update
+    cat = Cat.find(params[:id])
+    params[:cat].each do |key, value|
+      cat[key.to_sym] = value
+    end
+    cat.save
+    redirect_to cats_url
+  end
 end
