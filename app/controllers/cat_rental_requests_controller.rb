09ff9198a0010a65c858cb2bb36a76fdcdd917f9
Samir and Matt <samir+matt@appacademy.io>
2013-07-10 15:48:54 -0400

    Add CatRentalRequestsController#destroy

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -4,8 +4,8 @@ class CatRentalRequestsController < ApplicationController
   end
 
   def show
-    @cat = Cat.find_by_id(params[:cat_id])
     @crr = CatRentalRequest.find(params[:id])
+    @cat = Cat.find(@crr.cat.id)
   end
 
   def new
@@ -35,5 +35,11 @@ class CatRentalRequestsController < ApplicationController
     end
     crr.save
     redirect_to cat_cat_rental_requests_url
+  end
+
+  def destroy
+    cat_id = CatRentalRequest.find(params[:id]).cat_id
+    CatRentalRequest.destroy(params[:id])
+    redirect_to cat_cat_rental_requests_url(cat_id)
   end
 end
