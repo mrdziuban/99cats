37eec15b90fae5feca568322167bca34fe146669
Samir and Matt <samir+matt@appacademy.io>
2013-07-10 13:05:48 -0400

    Add CatRentalRequestsController#new, #create, and #edit

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -7,4 +7,22 @@ class CatRentalRequestsController < ApplicationController
     @cat = Cat.find_by_id(params[:cat_id])
     @crr = CatRentalRequest.find(params[:id])
   end
+
+  def new
+    @cat = Cat.find(params[:cat_id])
+  end
+
+  def create
+    CatRentalRequest.create(params[:cat_rental_request])
+    redirect_to cat_cat_rental_requests_url
+  end
+
+  def edit
+    @cat = Cat.find(params[:cat_id])
+    @crr = CatRentalRequest.find(params[:id])
+  end
+
+  def update
+
+  end
 end
